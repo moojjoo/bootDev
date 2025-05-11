200f2112103764df366ee4b4fa9496241a653400
Robert <robertdannelly@gmail.com>
2025-05-11 15:49:35 -0400

    Boot.Dev repo

---
+++ boot-dev-linux/commands.sh
@@ -0,0 +1,7 @@
+export PATH="$PATH:/home/moojjoo/worldbanc/private/bin"
+
+'/home/moojjoo/worldbanc/private/transactions'
+
+bash command 
+
+Run ps aux | head -n 1 to see the header row of the ps aux table.
